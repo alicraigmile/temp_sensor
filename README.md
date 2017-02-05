ad517da3e34ec3433b4eaa459bed15d0932727b1
Ali Craigmile <ali@craigmile.com>
2017-02-05 11:02:55 +0000

    removed test/splash screen added docs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # temp_sensor
+
+## Dependencies
+
+###Arduino libraries
+
+Adafruit_GFX by Adafruit: v1.1.5
+Adafruit Unified Sensor: v1.0.2
+DHT sensor library by Adafruit: v1.2.3
+
+##Gotchas
+
+* I could not get v1.3.0 of the DHT sensor library to workwork! 
--- temp_sensor.ino
+++ temp_sensor.ino
@@ -53,20 +53,6 @@ void setup()   {
   // Clear the buffer.
   display.clearDisplay();
 
-  // text display tests
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("Hello, world!");
-  display.setTextColor(BLACK, WHITE); // 'inverted' text
-  display.println(3.141592);
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.print("0x"); display.println(0xDEADBEEF, HEX);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
 }
 
 void readTempEtc() {
